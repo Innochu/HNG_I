4e8eb8f9b3607adea1e96ee403d2d622dc288cf4
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2024-06-21 02:08:53 +0100

    IPReset/controller

--- HNG/HNG/Controllers/WelcomeController.cs
+++ HNG/HNG/Controllers/WelcomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using System.Net.NetworkInformation;
 
 namespace HNG.Controllers
 {
@@ -7,14 +8,14 @@ namespace HNG.Controllers
     [ApiController]
     public class GreetingsController : ControllerBase
     {
-      
+
 
         [HttpPost]
         public IActionResult Intro(Model model)
         {
             string greeting;
             // Get the client's IP address
-            string ipAddress = HttpContext.Connection.RemoteIpAddress.ToString();
+            string ipAddress = GetServerPrivateIP();
 
 
             // Get the current hour
@@ -25,7 +26,7 @@ namespace HNG.Controllers
             {
                 greeting = "Good morning!";
             }
-            else if (hour < 17) 
+            else if (hour < 17)
             {
                 greeting = "Good afternoon!";
             }
@@ -35,7 +36,34 @@ namespace HNG.Controllers
             }
 
             // Return a JSON object with greeting and IP address
-            return Ok(new { message = $"{greeting}, {model.Name}", ipAddress = $"Your Public IP Address is {ipAddress}" });
+            return Ok(new { message = $"{greeting}, {model.Name}", ipAddress = $"Your IP Address is {ipAddress}" });
+        }
+
+        public static string GetServerPrivateIP()
+        {
+            string privateIP = "";
+            NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface network in networkInterfaces)
+            {
+                if (network.OperationalStatus == OperationalStatus.Up &&
+                    network.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                {
+                    IPInterfaceProperties properties = network.GetIPProperties();
+
+                    foreach (UnicastIPAddressInformation address in properties.UnicastAddresses)
+                    {
+                        if (address.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork &&
+                            !IPAddress.IsLoopback(address.Address))
+                        {
+                            privateIP = address.Address.ToString();
+                            return privateIP;
+                        }
+                    }
+                }
+            }
+
+            return privateIP;
         }
     }
 }
